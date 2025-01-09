d0503aa05f20756e84faed2baa749e87615f6fbf
Valen2525 <juroseror@unal.edu.co>
2025-01-09 15:41:03 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,18 +1,18 @@
 package comunicacion;
 
 public class Fabula extends Escrito{
-    String ensenanzas;
+    String ensenanza;
     String interpretacion;
 
     public Fabula(String origen, String titulo, String autor, int paginas, String ensenanzas, String interpretacion){
         super(origen, titulo, autor, paginas);
-        this.ensenanzas = ensenanzas;
+        this.ensenanza = ensenanzas;
         this.interpretacion = interpretacion;
     }
     public void setIntepretacion(String inter){this.interpretacion=inter;}
     public String getInterpretacion(){return interpretacion;}
-    public void setEnsenanzas(String ense){this.ensenanzas = ense;}
-    public String getEnsenanzas(){return ensenanzas;}
+    public void setEnsenanza(String ense){this.ensenanza = ense;}
+    public String getEnsenanza(){return ensenanza;}
 
     public int palabrasTotales(int paginas){
         return (int)paginas*1;
@@ -23,7 +23,7 @@ public class Fabula extends Escrito{
     }
     public String toString(){
         return super.getOrigen()+"/n"+super.getTitulo()+"/n"+super.getAutor()+"/n"+
-        super.getPaginas()+"/n"+getAutor()+"/n"+getEnsenanzas();
+        super.getPaginas()+"/n"+getAutor()+"/n"+getEnsenanza();
     }
 
 }
